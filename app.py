789a2830b4d85cd2506b7e99c9523cdf81222a8c
cbezaitis <45313684+babis12@users.noreply.github.com>
2020-06-10 21:17:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import settings
 import sys,os
 import pymysql as db
+import copy 
 
 def connection():
     ''' User this function to create your connections '''
@@ -21,6 +22,7 @@ def classify_review(reviewid):
     # Create a cursor on the connection
     cur=con.cursor()
     stringReview=str(reviewid) # kano to reviewid na eimai sigouros
+    
     sql= """
             SELECT r.text
             FROM reviews r
@@ -29,9 +31,25 @@ def classify_review(reviewid):
     try : 
         cur.execute(sql)
         results=cur.fetchall()
-        textOfReview=results # pira to text tis Review
+        textOfReview=results
+        # pira to text tis Review
+        # if results ==():
+        #     return [("This Review Id is wrong",)]
     except :
         print("error")
+        
+
+    sql= """
+            SELECT b.name
+            FROM reviews r,business b
+            WHERE r.review_id='""" +stringReview + "'AND b.business_id=r.business_id;"
+
+    try : 
+        cur.execute(sql)
+        results=cur.fetchall()
+        biznaName=results # pira to text tis Review
+    except :
+        print("error2")
 
     sql= """
             SELECT *
@@ -58,63 +76,85 @@ def classify_review(reviewid):
         print("error")
     # Apo sql exo parei negterm, posterm , textOfReview
 
+    reReview=textOfReview 
     textOfReview=str(textOfReview[0][0])
 
     print("\n to text tou review")
     print(textOfReview)
     print("\n\n\n")
 
-    ngrammata=extract_ngrams(list(textOfReview.split()),3)
 
-    print("\n ngrammata")
-    print(ngrammata)
-    print("\n\n\n")
 
-    listOfPosTerm = [a[0] for a in posterm]
 
-    print("\nlist of posterm")
-    print(listOfPosTerm)
-    print("\n\n\n")
     kalaSxolia= []
-    for i in range(3,0,-1):
-        ngrammata=extract_ngrams(list(textOfReview.split()),i)
+    listOfPosTerm = [a[0] for a in posterm]
+    for i in range(3,0,-1):  
+        ngrammata=extract_ngrams(textOfReview,i)
         kalaSxolia.append(list(set(ngrammata).intersection(listOfPosTerm)))
+    
+
+    kakaSxolia= []
+    listOfNegTerm = [a[0] for a in negterm]
+    print("\nKalaSxolia")
+    print(listOfNegTerm)
+    print("\n\n\n")
+    for i in range(3,0,-1):  
+        ngrammata=extract_ngrams(textOfReview,i)
+        kakaSxolia.append(list(set(ngrammata).intersection(listOfNegTerm)))
+
     print("\nKalaSxolia")
     print(kalaSxolia)
     print("\n\n\n")
-    # sostaKalaSxolia = kalaSxolia
-    # for sublista in reversed(kalaSxolia):
-    #     for itema in sublista:
-    #         for substring in itema.split():
-    #             for substring1 in itema.split():
-    #                 if substring==substring1:
-    #                     for each in kalaSxolia:
-    #                         for eachItem in each:
-    #                             if substring==eachItem:
-    #                                 kalaSxolia.each.remove(eachItem)
 
-
-  
-
-
-    print("\n Kala Sxolia")
-    print(kalaSxolia)
+    print("\nKaKaSxolia")
+    print(kakaSxolia)
     print("\n\n\n")
 
-    return textOfReview
+    kainouriaKalaSxolia= []
+    for sublist in kalaSxolia:
+        for item in sublist:
+            for item1 in item.split():
+                kainouriaKalaSxolia.append(item1)
 
+    kainouriaKalaSxolia = list(set(kainouriaKalaSxolia))
+    print("\nkainouriaKalaSxolia")
+    print(kainouriaKalaSxolia)
+    print("\n\n\n")
 
-def extract_ngrams(wordlist, n):   
-    ngrams = []
-    sostiLista= []    
-    for i in range(len(wordlist)-(n-1)):
-        ngrams.append(wordlist[i:i+n])
+    kainouriaKakaSxolia= []
+    for sublist in kakaSxolia:
+        for item in sublist:
+            for item1 in item.split():
+                kainouriaKakaSxolia.append(item1)
 
-    for sublist in ngrams:
-        sumOfStrings=" ".join(sublist)
-        sostiLista.append(sumOfStrings)
-    return sostiLista
+    kainouriaKakaSxolia = list(set(kainouriaKakaSxolia))
+    print("\nkainouriaKakaSxolia")
+    print(kainouriaKakaSxolia)
+    print("\n\n\n")
 
+    METRITIS=len(kainouriaKalaSxolia)-len(kainouriaKakaSxolia)
+    print("\n")
+    print(METRITIS)
+    print("\n\n\n")
+    if METRITIS>0:
+        result="kalo sxolio"
+    else:
+        result="kako sxolio"
+    
+    return [("Business Name","Text of Review","Kalo / kako sxolio"),(str(biznaName[0][0]),str(reReview[0][0]),str(result))] 
+
+def extract_ngrams(text, num): 
+    ngrams = [] 
+    if num==3:
+        m=-1
+    elif num==2:
+        m=0
+    else:
+        m=1  
+    void = text.count(' ') 
+    for i in range(void + m): 
+        ngrams.append(' '.join(text.split()[i:i+num])) 
+    return ngrams 
 
 
 def updatezipcode(business_id,zipcode):
