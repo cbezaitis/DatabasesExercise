f1636c32750c3dafb96f7b3e0165e5c20f82bdf3
cbezaitis <45313684+babis12@users.noreply.github.com>
2020-06-12 15:34:25 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -76,9 +76,9 @@ def classify_review(reviewid):
     re_review=text_of_review 
     text_of_review=str(text_of_review[0][0])
 
-    # print("\n to text tou review")
-    # print(text_of_review)
-    # print("\n\n\n")
+    print("\n to text tou review")
+    print(text_of_review)
+    print("\n\n\n")
 
 
 
@@ -87,26 +87,29 @@ def classify_review(reviewid):
     list_of_posterm = [a[0] for a in posterm]
     for i in range(3,0,-1):  
         ngrammata=extract_ngrams(text_of_review,i)
+        print("\n to text tou review")
+        print(ngrammata)
+        print("\n\n\n")
         positive.append(list(set(ngrammata).intersection(list_of_posterm)))
     
 
     negative= []
     list_of_negterm = [b[0] for b in negterm]
-    # print("\nKalaSxolia")
-    # print(list_of_negterm)
-    # print("\n\n\n")
+    print("\nKalaSxolia")
+    print(list_of_negterm)
+    print("\n\n\n")
     
     for i in range(3,0,-1):  
         ngrammata=extract_ngrams(text_of_review,i)
         negative.append(list(set(ngrammata).intersection(list_of_negterm)))
 
-    # print("\nKalaSxolia")
-    # print(positive)
-    # print("\n\n\n")
+    print("\nKalaSxolia")
+    print(positive)
+    print("\n\n\n")
 
-    # print("\nKaKaSxolia")
-    # print(negative)
-    # print("\n\n\n")
+    print("\nKaKaSxolia")
+    print(negative)
+    print("\n\n\n")
 
     positive_clean= []
     for sublist in positive:
@@ -126,14 +129,14 @@ def classify_review(reviewid):
                 negative_clean.append(item1)
 
     negative_clean = list(set(negative_clean))
-    # print("\nkainouriaKakaSxolia")
-    # print(negative_clean)
-    # print("\n\n\n")
+    print("\nkainouriaKakaSxolia")
+    print(negative_clean)
+    print("\n\n\n")
 
     review_counter=len(positive_clean)-len(negative_clean)
-    # print("\n")
-    # print(review_counter)
-    # print("\n\n\n")
+    print("\n")
+    print(review_counter)
+    print("\n\n\n")
     if (review_counter > 0):
         result="This is good!"
     else:
@@ -163,15 +166,16 @@ def updatezipcode(business_id,zipcode):
     
     # Create a cursor on the connection
     cur=con.cursor()
-
-    # sql = "SELECT EXISTS " +"("+ " SELECT "+"*"+" FROM business b WHERE business_id='"+business_id+"');"
-    # try : 
-    #     cur.execute(sql)
-    #     results=cur.fetchall()
-    #     if(results==0):
-    #         return [("there is no such business",),]
-    # except :
-    #     print("error")
+    sql = "SELECT EXISTS " +"("+ " SELECT "+"*"+" FROM business b WHERE business_id='"+str(business_id)+"');"
+    try : 
+        cur.execute(sql)
+        results=cur.fetchall()
+        print("To result tou prwtou SQL")
+        print(results)
+        if(results[0][0]==0):
+            return [("there is no such business",),]
+    except :
+        print("error")
 
     sql = "UPDATE  yelp.business SET zip_code = "+zipcode+" WHERE business_id='"+business_id+"';"
     print("to zip code : =%s",zipcode)
@@ -209,6 +213,7 @@ def selectTopNbusinesses(category_id,n):
     
     string_of_category=str(category_id)
 
+
     sql= """
             SELECT DISTINCT b.business_id, COUNT(rpn.positive)
             FROM  business b, reviews_pos_neg rpn, reviews r, business_category bc , category c
@@ -220,67 +225,121 @@ def selectTopNbusinesses(category_id,n):
 
     try : 
         cur.execute(sql)
-        results=cur.fetchall()
+        results=cur.fetchall() # orea tha itane na douleue h fetchmany
         best_of_all=results 
     except :
         print("error")
+        return["PROBLEM"]
     
 
-    print(best_of_all)
-    print(best_of_all[0][0])
-    print(best_of_all[0][1])
-    print(best_of_all[1][0])
-    print(best_of_all[1][1])
     best_of_N= list()
+    kati=("Business ID","Number of Positive Reviews")
+    best_of_N.append(kati)  # Vazo tin kefali tou table pou tha paei stin othoni
     for i in range(int(n)):
-        best_of_N.append(best_of_all[i])
+        kati=(best_of_all[i][0],str(best_of_all[i][1]))
+        best_of_N.append(kati) # simplirono to upoloipo table
 
-    print(best_of_N)
-    # steile0=(best_of_all[i][0] for i in range(int(n)))
-    # steile1=(best_of_all[i][1] for i in range(int(n)))
-    return [("Business ID","Number Of Positive Reviews"),([best_of_all[i][0] for i in range(int(n))],[best_of_all[i][1] for i in range(int(n))])]
+    print(best_of_N)    # dokimastiki ektiposi sto terminal 1
+
+    best_of_N=tuple(best_of_N)  # metatropi tis listas se tupple 
+                                 # gia ektiposi sto site
+    
+    print("\n\n\n")            
+    print(best_of_N) # domikastiki ektiposi sto terminal 2
+    # return [best_of_N]
+    return best_of_N
 
 def traceUserInfuence(userId,depth):
+    depth=int(depth)
     # Create a new connection
     con=connection()
 
     # Create a cursor on the connection
     cur=con.cursor()
 
+    print(sql_different_business(cur,userId))
+    print("\n\n\n")
+
+    friends_business=sql_different_business(cur,userId)
+
+    list_of_lists=[]
+    for i in range(depth):
+        list_of_lists.append([])
+
+    print("\n\n\n")
+    print(friends_business)
+    # Vazoume se mia lista apo listes tous vathos 1 epireasmenous filous kai tis epixeiriseis 
+    list_of_lists[0].extend([list(sql_different_business(cur,userId))])
+
+    print("\nfilos id 0 : \n")
+    print(list_of_lists[0][0][0][0])
+    print("\n\n\n")
+    
+    print("\nfilos id 1: \n")
+    print(list_of_lists[0][0][1][0])
+    print("\n\n\n")
+
+    print("\nbuzna id 0: \n")
+    print(list_of_lists[0][0][0][1])
+    print("\n\n\n")
+
+    print("\nbuzna id 1: \n")
+    print(list_of_lists[0][0][1][1])
+    print("\n\n\n")
+
+    print(sql_same_business(cur,list_of_lists[0][0][0][0],list_of_lists[0][0][0][1]))
+    print("\n\n\n")
+
+    for i in range(len(list_of_lists[0][0])):
+        print("\n")
+        print(sql_same_business(cur,list_of_lists[0][0][i][0],list_of_lists[0][0][i][1]))
+        print("\n")
+
+
+
     return [("user_id",),]
 
 
 
-def sql_same_business(cur,friend_as_user,same_business_id):
-
-    sql= """
-            SELECT f.friend_id, u.user_id, ru.business_id, ru.date, rf.date
-            FROM friends f, user u, reviews ru, reviews rf
-            WHERE u.user_id = f.user_id AND ru.business_id = rf.business_id AND u.user_id = ru.user_id 
-							AND rf.user_id = f.friend_id AND ru.date < rf.date AND u.user_id = """+friend_as_user+" AND rf.business_id = "+same_business_id+"""
-            group by ru.business_id;
-         """
-
-    try : 
-        cur.execute(sql)
-        results=cur.fetchall()
-        best_of_all=results 
-    except :
-        print("error")
-
 def sql_different_business(cur,user_Id):
-    
+    string_of_user = str(user_Id)
+    print(string_of_user)
+
     sql= """
-            SELECT f.friend_id, u.user_id, ru.business_id, ru.date, rf.date
+            SELECT f.friend_id, ru.business_id
             FROM friends f, user u, reviews ru, reviews rf
             WHERE u.user_id = f.user_id AND ru.business_id = rf.business_id AND u.user_id = ru.user_id 
-							AND rf.user_id = f.friend_id AND ru.date < rf.date AND u.user_id = """+user_Id+"""
-            group by ru.business_id;
+                                        AND rf.user_id = f.friend_id AND ru.date < rf.date AND u.user_id = '"""+string_of_user+"""' ;
          """
 
     try : 
         cur.execute(sql)
         results=cur.fetchall()
-        best_of_all=results 
+        friends=results 
     except :
-        print("error")
+        print("errooooor")
+        return None
+           
+    return results
+
+def sql_same_business(cur,friend_as_user,same_business_id):
+    
+    string_business = str(same_business_id)
+    friend_as_user  = str(friend_as_user)
+
+    sql= """
+            SELECT f.friend_id
+            FROM friends f, user u, reviews ru, reviews rf
+            WHERE u.user_id = f.user_id AND ru.business_id = rf.business_id AND u.user_id = ru.user_id 
+                            AND rf.user_id = f.friend_id AND ru.date < rf.date AND u.user_id = '"""+friend_as_user+"' AND rf.business_id = '"+string_business+"""';
+         """
+
+    try : 
+        cur.execute(sql)
+        results=cur.fetchall()
+        influenced=results 
+    except :
+        print("error323423")
+        return None
+
+    return results 
